6775b7452b932b03fcae16196a974ccc77cd2d8a
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2024-12-19 11:41:26 -0300

    feat: add listerner event click on dropdown menu to close

--- src/components/menus/Mobile/index.tsx
+++ src/components/menus/Mobile/index.tsx
@@ -1,28 +1,42 @@
-
 // External library
-import { useState } from 'react'
+import { useState, useEffect, useRef } from "react";
 import { IoMenuOutline } from "react-icons/io5";
 
 // Components
 import DropDownMenu from "../subcomponent/DropDown";
 
 // Types
-import { NavListProps } from '../../../types/Navigation'
+import { NavListProps } from "../../../types/Navigation";
 
 // Styles
 import { SandWichIcon } from "./styles";
 
 export default function MenuMobile({ links }: NavListProps) {
   const [openMenu, setOpenMenu] = useState<boolean>(false);
+  const menuRef = useRef<HTMLDivElement>(null);
 
   const handleOpenMenu = () => {
-    setOpenMenu(prev => !prev);
+    setOpenMenu((prev) => !prev);
   };
 
+  const handleClickOutside = (event: MouseEvent) => {
+    if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+      setOpenMenu(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <SandWichIcon>
+    <SandWichIcon ref={menuRef}>
       <IoMenuOutline size="3rem" color="white" onClick={handleOpenMenu} />
-      {openMenu && <DropDownMenu links={links}/>}
+      {openMenu && <DropDownMenu links={links} />}
     </SandWichIcon>
   );
 }
