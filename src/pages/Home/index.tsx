fc1a979469fc237422270635e6832cade01c1bc1
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2024-12-23 23:10:54 -0300

    refactor: better organize the import of components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,15 @@
 // Components
-import Footer from "../../components/Footer";
 import Header from "../../components/Header";
-import Contact from "../../components/sections/Contact";
-import Editions from "../../components/sections/Editions";
 import HeroBanner from "../../components/sections/HeroBanner";
-import FAQs from "../../components/sections/FAQs";
-import Highlights from "../../components/sections/Highlights";
-import Locations from "../../components/sections/Locations";
-import Programming from "../../components/sections/Programming";
 import Prologue from "../../components/sections/Prologue";
 import Supporters from "../../components/sections/Supporters";
+import Programming from "../../components/sections/Programming";
+import Locations from "../../components/sections/Locations";
+import Highlights from "../../components/sections/Highlights";
+import Contact from "../../components/sections/Contact";
+import Editions from "../../components/sections/Editions";
+import FAQs from "../../components/sections/FAQs";
+import Footer from "../../components/Footer";
 
 // Styles
 import { Main } from "./styles";
