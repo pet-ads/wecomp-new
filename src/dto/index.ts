075c3a349e14397df50b5f9499b8b529302020aa
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-01-14 22:19:17 -0300

    feat: crate dto directory for centralize imports of components in main

---
+++ src/dto/index.ts
@@ -0,0 +1,13 @@
+
+export { default as Header } from "../components/Header";
+export { default as HeroBanner } from "../components/sections/HeroBanner";
+export { default as Prologue } from "../components/sections/Prologue";
+export { default as Supporters } from "../components/sections/Supporters";
+export { default as Programming } from "../components/sections/Programming";
+export { default as Locations } from "../components/sections/Locations";
+export { default as Marathon } from "../components/sections/Marathon";
+export { default as Highlights } from "../components/sections/Highlights";
+export { default as Contact } from "../components/sections/Contact";
+export { default as Editions } from "../components/sections/Editions";
+export { default as FAQs } from "../components/sections/FAQs";
+export { default as Footer } from "../components/Footer";
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,19 +1,21 @@
 // Components
-import Header from "../../components/Header";
-import HeroBanner from "../../components/sections/HeroBanner";
-import Prologue from "../../components/sections/Prologue";
-import Supporters from "../../components/sections/Supporters";
-import Programming from "../../components/sections/Programming";
-import Locations from "../../components/sections/Locations";
-import Highlights from "../../components/sections/Highlights";
-import Contact from "../../components/sections/Contact";
-import Editions from "../../components/sections/Editions";
-import FAQs from "../../components/sections/FAQs";
-import Footer from "../../components/Footer";
+import {
+  Header,
+  HeroBanner,
+  Prologue,
+  Supporters,
+  Programming,
+  Marathon,
+  Highlights,
+  Locations,
+  Contact,
+  Editions,
+  FAQs,
+  Footer,
+} from "../../dto";
 
 // Styles
 import { Main } from "./styles";
-import Marathon from "../../components/sections/Marathon";
 
 export default function Home() {
   return (
