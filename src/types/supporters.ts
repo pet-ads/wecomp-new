a22d98dc9eb3ef2de028094a006e3b7c054cf6fb
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-01-31 00:24:36 -0300

    feat: add new atributtes in supporters

--- src/types/supporters.ts
+++ src/types/supporters.ts
@@ -1,10 +1,12 @@
 export interface SupportersProps {
-    id: string | number;
-    nameSupporter: string;
-    logoAlternativeText: string;
-    logoPath: string;
+  id: string | number;
+  nameSupporter: string;
+  logoAlternativeText: string;
+  logoPath: string;
+  link: string;
+  width: string;
 }
 
 export interface SupportersScetionProps {
-    eventSupporters: SupportersProps[];
+  eventSupporters: SupportersProps[];
 }
