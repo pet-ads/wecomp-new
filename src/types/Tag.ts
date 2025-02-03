caafd0fa8c6e8b3f392d938b304ea80d9061296e
CarolinaBis <carolbis09@gmail.com>
2025-02-03 16:22:58 -0300

    feat: Create the interfaces in the tags components

---
+++ src/types/Tag.ts
@@ -0,0 +1,11 @@
+export interface DifficultyTagProps {
+  level: "livre" | "básico" | "intermediário";
+}
+
+export interface AvailabilityTagProps {
+  status: "encerrado!" | "últimas vagas" | "disponível";
+}
+
+export interface QualificationProps {
+  level: "estágio" | "trainee" | "júnior" | "pleno";
+}
