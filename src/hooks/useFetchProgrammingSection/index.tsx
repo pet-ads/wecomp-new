d2cb6462f9a5e562bcaa925f0df215128d4c3ac1
Matheus <matheusg.farias@hotmail.com>
2025-02-08 17:34:21 -0300

    fix: correcting hook export useFetchProgrammingSection

--- src/hooks/useFetchProgrammingSection/index.tsx
+++ src/hooks/useFetchProgrammingSection/index.tsx
@@ -4,6 +4,6 @@ import {ProgrammingSectionProps} from "../../types/Programming";
 // JSON
 import programming from "../../../public/data/programming.json";
 
-export default function useFetchSupportersSection(): ProgrammingSectionProps {
+export default function useFetchProgrammingSection(): ProgrammingSectionProps {
     return programming;
 }
