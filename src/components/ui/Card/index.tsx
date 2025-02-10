f418289670d69aff95a526ed16b34bb94f8d12e6
Matheus <matheusg.farias@hotmail.com>
2025-02-10 15:37:23 -0300

    feat: separating the card into styled-containers

--- src/components/ui/Card/index.tsx
+++ src/components/ui/Card/index.tsx
@@ -14,11 +14,36 @@ import {
   Image,
   LinkButtonContainer,
   InfoContainer,
+  TopContainer,
+  ImageContainer,
+  MiddleContainer,
+  BottomContainer,
+  VacanciesContainer,
+  ButtonsContainer,
 } from "./styles";
 
-import ButtonContainer  from "../../../styles/ButtonContainer";
+import ButtonContainer from "../../../styles/ButtonContainer";
 
-export default function Card({ children, name, image, imageDescription, bio, author, date, time, link, active, vacancies, location, status, classification, description }:CardComponentProps) {
+// Components
+import StrongParagraph from "../StrongParagraph";
+
+export default function Card({
+  children,
+  name,
+  image,
+  imageDescription,
+  bio,
+  author,
+  date,
+  time,
+  link,
+  active,
+  vacancies,
+  location,
+  status,
+  classification,
+  description,
+}: CardComponentProps) {
   const [showBiography, setshowBiography] = useState(false);
 
   const isTechnicalVisit = name.toLowerCase().includes("visita técnica");
@@ -29,37 +54,46 @@ export default function Card({ children, name, image, imageDescription, bio, aut
 
   return (
     <Container>
+      <TopContainer>
+        <InfoContainer>
+          <Title>{name}</Title>
+          <StrongParagraph>Local:</StrongParagraph> {location}
+          <StrongParagraph>Data:</StrongParagraph> {date}
+          <StrongParagraph>Horário:</StrongParagraph> {time}
+          <StrongParagraph>Autor:</StrongParagraph> {author}
+          <StrongParagraph>Status:</StrongParagraph> {status}
+          <StrongParagraph>Classificação:</StrongParagraph> {classification}
+        </InfoContainer>
 
-      <Title>{name}</Title>
+        <ImageContainer>
+          <Image src={image} alt={imageDescription} />
+        </ImageContainer>
+      </TopContainer>
 
-      <Description>{showBiography ? bio : description}</Description>
+      <MiddleContainer>
+        <Description>{showBiography ? bio : description}</Description>
+      </MiddleContainer>
 
-      
+      <BottomContainer>
+        <VacanciesContainer>
+          <StrongParagraph>Vagas:</StrongParagraph> {vacancies}
+        </VacanciesContainer>
 
-      <InfoContainer>
-        {author && <p><strong>Autor:</strong> {author}</p>}
-        {date && <p><strong>Data:</strong> {date}</p>}
-        {time && <p><strong>Horário:</strong> {time}</p>}
-        {location && <p><strong>Local:</strong> {location}</p>}
-        {status && <p><strong>Status:</strong> {status}</p>}
-        {classification && <p><strong>Classificação:</strong> {classification}</p>}
-        {vacancies && <p><strong>Vagas disponíveis:</strong> {vacancies}</p>}
-      </InfoContainer >
-
-      {image && (
-        <Image src={image} alt={imageDescription} />
-      )}
-
-      {link && (
-        <LinkButtonContainer href={link} target="_blank" rel="noopener noreferrer">
-          Inscrever-se
-        </LinkButtonContainer>
-      )}
-      {!isTechnicalVisit && (
-        <ButtonContainer onClick={() => setshowBiography(!showBiography)}>
-          {showBiography ? "Ver informações do evento" : "Ver biografia"}
-        </ButtonContainer>
-      )}
+        <ButtonsContainer>
+          <LinkButtonContainer
+            href={link}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Inscrever-se
+          </LinkButtonContainer>
+          {!isTechnicalVisit && (
+            <ButtonContainer onClick={() => setshowBiography(!showBiography)}>
+              {showBiography ? "Ver informações do evento" : "Ver biografia"}
+            </ButtonContainer>
+          )}
+        </ButtonsContainer>
+      </BottomContainer>
 
       {children}
     </Container>
