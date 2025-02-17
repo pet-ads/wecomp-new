1997b4b43603a66b95b67b82830c8831a88c708b
Matheus <matheusg.farias@hotmail.com>
2025-02-17 15:55:21 -0300

    feat: add figma styles and responsive styles

--- src/components/sections/Programming/styles.ts
+++ src/components/sections/Programming/styles.ts
@@ -1,30 +1,129 @@
-// External Library
+// External library
 import styled from "styled-components";
 
-// Styles
-import {
-  SectionContainer,
-  SectionContent,
-} from "../../../styles/SectionContainer";
-
-export const ProgrammingContainer = styled(SectionContainer)`
-background: var(--background-color);`;
-
-export const ProgrammingContent = styled(SectionContent)`
-background: var(--background-color);`;
-
-export const ProgrammingCardContainer = styled.div`
+export const Container = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  background-color: black;
+
+  width: 90%;
+  min-width: 90%;
+  max-width: 90%;
+
+  height: fit-content;
+  min-height: 90%;
+  max-height: 90%;
+
+  margin: 5%;
+  padding: 1rem 2rem;
+  gap: 1rem;
+  border-radius: 1rem;
+  border: 2px solid black;
+  background-color: var(--primary-text-color);
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+  overflow: hidden;
+  aspect-ratio: 16 / 9;
+
+  @media (max-width: 768px) {
+    width: 100%;
+    min-width: 100%;
+    max-width: 100%;
+    margin: 0;
+    padding: 1rem;
+    aspect-ratio: auto;
+  }
+`;
+
+export const Title = styled.h2`
+  font-size: 31px;
+
+  @media (max-width: 768px) {
+    font-size: 24px;
+  }
+`;
+
+export const Description = styled.p`
+  text-align: center;
+  font-size: 16px;
+
+  @media (max-width: 768px) {
+    font-size: 14px;
+  }
+`;
+
+export const InfoContainer = styled.div`
+  margin-bottom: 1rem;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  align-items: center;
+`;
+
+export const Image = styled.img`
+  width: auto;
+`;
+
+export const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   width: 100%;
 
-  min-height: 40vh;
-
-  padding: 2rem 0;
-
-  margin: 2rem 0;
-
-  background: var(--background-color);
+  @media (max-width: 768px) {
+    flex-direction: column;
+    gap: 1rem;
+  }
 `;
+
+export const ImageContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const DescriptionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const FooterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  margin-top: 1rem;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    gap: 1rem;
+  }
+`;
+
+export const VacanciesContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: left;
+  gap: 0.5rem;
+`;
+
+export const ButtonsContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  justify-content: flex-end;
+`;
+
+export const AuthorContainer = styled.div`
+  display: inline-block;
+`;
+
+export const StatusInfoContainer = styled.div`
+  display: flex;
+  gap: 1rem;
+`;
+
+export const StatusContainer = styled.div``;
+
+export const ClassificationContainer = styled.div``;
