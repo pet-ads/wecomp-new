4320bc1b95f7687965f41eca53b476fc373b7e42
CarolinaBis <carolbis09@gmail.com>
2025-02-19 14:14:30 -0300

    feat:Add information about InterIf marathon to the index

--- src/components/sections/Marathon/index.tsx
+++ src/components/sections/Marathon/index.tsx
@@ -4,26 +4,52 @@ import ArrowButton from "../../ui/ArrowButton";
 
 // Hooks
 import useSectionRefs from "../../../hooks/useSectionRefs";
+import useMarathonData from "../../../hooks/useMarathonData";
+
 
 // Styles
 import {
   MarathonCardContainer,
   MarathonContainer,
   MarathonContent,
+  MarathonCard,
+  ScheduleTable,
 } from "./styles";
+import Text from "../../ui/Text";
+import RedirectButton from "../../ui/RedirectButton";
 
 export default function Marathon() {
   const { sections } = useSectionRefs();
+  const marathonData = useMarathonData();
+  const marathon = marathonData[0];
 
   return (
     <MarathonContainer>
       <MarathonContent ref={sections.Marathon}>
-        <SubTitle children="Maratona InterIF" />
+        <SubTitle>{marathon.title}</SubTitle>
         <MarathonCardContainer>
-          Carrossel of Marathon cards
+          <MarathonCard>
+            <Text children={marathon.definition} />  
+            <RedirectButton  link={marathon.link} children="Ver mais" />  
+          </MarathonCard>
+          
+          <MarathonCard>
+            <Text children="Cronograma" />
+            <ScheduleTable>
+              <tbody>
+                {marathon.schedule.map((event: { time: string; activity: string }, index: number) => (
+                  <tr key={index}>
+                    <td>{event.time}</td>
+                    <td>{event.activity}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </ScheduleTable>
+          </MarathonCard>
         </MarathonCardContainer>
-      <ArrowButton sectionRef={sections.Highlights} />
+        <ArrowButton sectionRef={sections.Highlights} />
       </MarathonContent>
     </MarathonContainer>
   );
 }
+
