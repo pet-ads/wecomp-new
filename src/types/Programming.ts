8cbfe74b7840c1edb2b4173cdd756176386bcaae
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-02-27 11:48:29 -0300

    refactor: create propertie to type event

--- src/types/Programming.ts
+++ src/types/Programming.ts
@@ -1,21 +1,23 @@
+export type TypeEventName = "Talk" | "Course" | "Opening" | "TechnicalVisit";
+
 export interface ProgrammingProps {
-    id: string | number;
-    name: string;
-    image: string;
-    imageDescription: string;
-    bio: string;
-    author: string;
-    date: string;
-    time: string;
-    link: string;
-    vacancies: string | number;
-    location: string;
-    status: string;
-    classification: string;
-    description: string;
-    programmingIcon: string;
+  id: string | number;
+  name: string;
+  image: string;
+  imageDescription: string;
+  bio: string;
+  author: string;
+  date: string;
+  time: string;
+  link: string;
+  vacancies: string | number;
+  location: string;
+  status: string;
+  classification: string;
+  description: string;
+  typeEvent: TypeEventName;
 }
 
 export interface ProgrammingSectionProps {
-    eventProgramming: ProgrammingProps[];
+  eventProgramming: ProgrammingProps[];
 }
