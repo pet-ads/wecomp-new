8d4b6349eee158e7525887cffec9152dd8335cee
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-03-05 17:46:41 -0300

    feat: create Difficulty  tag component

--- src/components/ui/tags/DifficultyTag/index.tsx
+++ src/components/ui/tags/DifficultyTag/index.tsx
@@ -2,16 +2,38 @@
 import { Container } from "./styles.ts";
 
 // Types
-import { TagProps } from "../../../../types/Tag";
+import { DifficultyEventProps } from "../../../../types/Tag";
 
-export default function DifficultyTag({
-  label,
-  textColor,
-  backgroundColor,
-}: TagProps) {
+export default function DifficultyTag({ label }: DifficultyEventProps) {
+  const getAvailabilityProperties = () => {
+    const options = {
+      Free: {
+        backgroundColor: "#DCF7E4",
+        textColor: "#1A800A",
+        translateText: "Livre",
+      },
+      Basic: {
+        backgroundColor: "#E3F0FF",
+        textColor: "#367AF5",
+        translateText: "Básico",
+      },
+      Intermediary: {
+        backgroundColor: "#FCF2D6",
+        textColor: "#EAB00D",
+        translateText: "Intermediário",
+      },
+    };
+    return options[label];
+  };
+
+  const tagProps = getAvailabilityProperties();
+
   return (
-    <Container backgroundColor={backgroundColor} textColor={textColor}>
-      {label}
+    <Container
+      backgroundColor={tagProps.backgroundColor}
+      textColor={tagProps.textColor}
+    >
+      {tagProps.translateText}
     </Container>
   );
 }
