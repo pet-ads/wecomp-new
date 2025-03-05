335d675401862242712655712b912dc14101001d
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-03-05 17:45:59 -0300

    feat: create Availibility tag component

--- src/components/ui/tags/AvailabilityTag/index.tsx
+++ src/components/ui/tags/AvailabilityTag/index.tsx
@@ -2,15 +2,34 @@
 import { Container } from "./styles.ts";
 
 // Types
-import { TagProps } from "../../../../types/Tag";
+import { AvailabilityProps } from "../../../../types/Tag";
 
-export default function AvailabilityTag({
-  label,
-  textColor,
-  backgroundColor,
-}: TagProps) {
+export default function AvailabilityTag({label}: AvailabilityProps) {
+  const getAvailabilityProperties = () => {
+    const options = {
+      Available: {
+        backgroundColor: "#DCF7E4",
+        textColor: "#1A800A",
+      },
+      LastVacancies: {
+        backgroundColor: "#FCF2D6",
+        textColor: "#EAB00D",
+      },
+      Closed: {
+        backgroundColor: "#F2E6E6",
+        textColor: "#FF6158",
+      },
+    };
+    return options[label];
+  };
+
+  const tagProps = getAvailabilityProperties();
+
   return (
-    <Container backgroundColor={backgroundColor} textColor={textColor}>
+    <Container
+      backgroundColor={tagProps.backgroundColor}
+      textColor={tagProps.textColor}
+    >
       {label}
     </Container>
   );
