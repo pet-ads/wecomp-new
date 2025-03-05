e778e974736fee78fe620ca58df5349d6e5eab0a
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-03-05 17:43:01 -0300

    feat: create interfaces to tag components

--- src/types/Tag.ts
+++ src/types/Tag.ts
@@ -1,5 +1,17 @@
-export type AvailabilityStatus = "Available" | "Last vacancies" | "Closed";
+export type AvailabilityStatus = "Available" | "LastVacancies" | "Closed";
 
 export type DifficultyEvent = "Free" | "Basic" | "Intermediary";
 
 export type OfferModality = "Trainee" | "Internship" | "Hiring";
+
+export interface AvailabilityProps {
+  label: AvailabilityStatus;
+}
+
+export interface DifficultyEventProps {
+  label: DifficultyEvent;
+}
+
+export interface OfferModalityProps {
+  label: OfferModality;
+}
