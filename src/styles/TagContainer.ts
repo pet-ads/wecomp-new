3aa1a17f05d71f21d812c5e87886ea843aedb5eb
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-03-05 17:45:02 -0300

    feat: create styled tag container to used with template

--- src/styles/TagContainer.ts
+++ src/styles/TagContainer.ts
@@ -9,9 +9,17 @@ export const TagContainer = styled.div<{
   align-items: center;
   justify-content: center;
 
+  width: 100%;
+
+  height: 100%;
+
   background-color: ${(props) =>
     props.backgroundColor || `var(--primary-text-color)`};
   color: ${(props) => props.textColor || `var(--secundary-text-color)`};
 
+  border-radius: 1rem;
+
   box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+
+  font-weight: bold;
 `;
