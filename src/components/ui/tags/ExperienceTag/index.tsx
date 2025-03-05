308c61aee538150d65a0e66669d26ed034fd9e25
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-03-05 18:02:43 -0300

    feat: create Experience tag component

--- src/components/ui/tags/ExperienceTag/index.tsx
+++ src/components/ui/tags/ExperienceTag/index.tsx
@@ -2,16 +2,38 @@
 import { Container } from "./styles.ts";
 
 // Types
-import { TagProps } from "../../../../types/Tag";
+import { OfferModalityProps } from "../../../../types/Tag";
 
-export default function ExpirienceTag({
-  label,
-  textColor,
-  backgroundColor,
-}: TagProps) {
-  return (
-    <Container backgroundColor={backgroundColor} textColor={textColor}>
-      {label}
-    </Container>
-  );
+export default function ExpirienceTag({ label }: OfferModalityProps) {
+    const getExperienceProperties = () => {
+      const options = {
+        Trainee: {
+          backgroundColor: "#DCF7E4",
+          textColor: "#1A800A",
+          translateText: "Treinee",
+        },
+        Internship: {
+          backgroundColor: "#E3F0FF",
+          textColor: "#367AF5",
+          translateText: "Estágio",
+        },
+        Hiring: {
+          backgroundColor: "#FCF2D6",
+          textColor: "#EAB00D",
+          translateText: "Contratação",
+        },
+      };
+      return options[label];
+    };
+  
+    const tagProps = getExperienceProperties();
+  
+    return (
+      <Container
+        backgroundColor={tagProps.backgroundColor}
+        textColor={tagProps.textColor}
+      >
+        {tagProps.translateText}
+      </Container>
+    );
 }
