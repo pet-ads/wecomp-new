1eecc38e0ff493621be509c04ae0c595e7b99477
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-03-05 12:17:16 -0300

    feat: create custom hook to change of content in programming card

---
+++ src/hooks/useToggleCardContent/index.tsx
@@ -0,0 +1,24 @@
+// External libraries
+import { useState } from "react";
+
+export default function useToggleCardContent(
+  description: string,
+  bio: string,
+  labelButtonText: string
+) {
+  const [labelButton, setLabelButton] = useState<string>(labelButtonText);
+  const [cardText, setCardText] = useState<string>(description);
+
+  const handleChangeCardText = () => {
+    setCardText((prev) => (prev === description ? bio : description));
+    setLabelButton((prev) =>
+      prev === "Ver biografia" ? "Ver descrição" : "Ver biografia"
+    );
+  };
+
+  return {
+    labelButton,
+    cardText,
+    handleChangeCardText,
+  };
+}
