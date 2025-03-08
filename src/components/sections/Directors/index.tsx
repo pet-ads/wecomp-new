9286c3027119d8eeb45d74ccf017218cd69eb6fc
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-03-08 01:37:05 -0300

    feat: create directors section

--- src/components/sections/Directors/index.tsx
+++ src/components/sections/Directors/index.tsx
@@ -1,5 +1,41 @@
+import ArrowButton from "../../ui/ArrowButton";
+import SubTitle from "../../ui/SubTitle";
 
+import useSectionRefs from "../../../hooks/useSectionRefs";
+
+import {
+  DirectorContainer,
+  DirectorContent,
+  DirectorLogo,
+  DirectorsLogoContainer,
+} from "./styles";
+import useFetchDirectorsSection from "../../../hooks/useFetchDirectorsSection";
 
 export default function Directors() {
-    return <>teste</>
+  const { sections } = useSectionRefs();
+  const data = useFetchDirectorsSection();
+
+  const handleRedirectOnClick = (url: string) => {
+    window.open(url, "_blank");
+  };
+
+  return (
+    <DirectorContainer ref={sections.directors}>
+      <DirectorContent>
+        <SubTitle children="Realizadores" />
+        <DirectorsLogoContainer>
+          {data.map((director) => (
+            <DirectorLogo
+              key={director.id}
+              src={director.logoPath}
+              alt={director.logoAlternativeText}
+              width={director.width}
+              onClick={() => handleRedirectOnClick(director.link)}
+            />
+          ))}
+        </DirectorsLogoContainer>
+      </DirectorContent>
+      <ArrowButton sectionRef={sections.programming} />
+    </DirectorContainer>
+  );
 }
