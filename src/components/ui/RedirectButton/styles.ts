69f61c8e3df7ce23dafb4aaee3efca720bbc02d4
vmacena <macena.v@aluno.ifsp.edu.br>
2025-03-14 07:47:57 -0300

    style: add margin-top to link button

--- src/components/ui/RedirectButton/styles.ts
+++ src/components/ui/RedirectButton/styles.ts
@@ -3,7 +3,7 @@ import ButtonContainer from "../../../styles/ButtonContainer.ts";
 
 const LinkButton = styled(ButtonContainer)`
     text-decoration: none;
-    
+     margin-top: 1.5rem;
 `;
 
 export default LinkButton;
