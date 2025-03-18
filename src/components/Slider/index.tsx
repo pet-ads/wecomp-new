64178428cfed6c7fe53d40d72de80c9e30937210
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-03-17 21:32:56 -0300

    feat: add method to event listener

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -8,13 +8,18 @@ export default function Slider<T>({ items, renderItem }: SliderProps<T>) {
   const [carouselWidth, setCarouselWidth] = useState<number>(0);
   const carouselContainerRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
+  const updateCarouselWidth = () => {
     if (carouselContainerRef.current) {
       setCarouselWidth(
         carouselContainerRef.current.scrollWidth -
           carouselContainerRef.current.offsetWidth
       );
     }
+  };
+
+  useEffect(() => {
+    updateCarouselWidth();
+    window.addEventListener("resize", updateCarouselWidth);
   }, []);
 
   return (
