4e8249162eb80f006adbdf5e3622e5db523a5f68
KG-S <k.gabriel@aluno.ifsp.edu.br>
2025-04-08 20:14:23 -0300

    refactor: adjusting the Coming Soon box

--- src/components/ui/SoonCard/styles.ts
+++ src/components/ui/SoonCard/styles.ts
@@ -5,19 +5,10 @@ export const SoonCardContainer = styled.div`
   align-items: center;
   justify-content: center;
 
-  width: 20rem;
+  width: 100%;
   height: 20rem;
-
-  background-color: var(--primary-text-color);
-  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.3);
+  
   border-radius: 1rem;
-
-  transition: transform 0.2s ease-in-out, box-shadow 0.2s ease-in-out;
-
-  &:hover {
-    transform: translateY(-5px);
-    box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.25);
-  }
 `;
 
 export const SoonText = styled.span`
