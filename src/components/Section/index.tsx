b47a3ca6358771843fc688f1f526e609985e6841
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-04-12 14:59:04 -0300

    feat: add id reference in component

--- src/components/Section/index.tsx
+++ src/components/Section/index.tsx
@@ -2,6 +2,6 @@ import { SectionProps } from "./types";
 
 import { Container } from "./styles";
 
-export const Section = ({ children }: SectionProps) => {
-  return <Container>{children}</Container>;
+export const Section = ({ children, sectionId }: SectionProps) => {
+  return <Container id={sectionId}>{children}</Container>;
 };
--- src/components/Section/types.ts
+++ src/components/Section/types.ts
@@ -1,3 +1,4 @@
 export type SectionProps = {
+  sectionId: string;
   children: React.ReactNode;
 };
