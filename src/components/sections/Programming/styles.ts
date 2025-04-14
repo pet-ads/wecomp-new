05a95fd632bc726de692cc3842b6cd8361230785
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-04-14 15:15:17 -0300

    refactor: adjust responsibility in Programming section

--- src/components/sections/Programming/styles.ts
+++ src/components/sections/Programming/styles.ts
@@ -1,9 +1,20 @@
 import styled from "styled-components";
 
-import { SectionContent } from "../../../styles/SectionContainer";
+export const ProgrammingContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 
-export const ProgrammingContent = styled(SectionContent)`
+  min-width: 80%;
+  max-width: 80%;
+
+  height: 100%;
+
   background: var(--background-color);
+
+  overflow-wrap: break-word;
+  line-height: 1.6;
 `;
 
 export const ProgrammingCardContainer = styled.div`
@@ -15,8 +26,6 @@ export const ProgrammingCardContainer = styled.div`
 
   min-height: 100%;
   max-height: 100%;
-
-  padding: 2rem 0;
 
   background-color: var(--background-color);
 `;
