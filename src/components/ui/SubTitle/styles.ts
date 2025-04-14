77548def71c074173e5a3f085e0b1028fcc55999
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-04-14 14:53:10 -0300

    refactor: responsibility with facepaint in subtitle

--- src/components/ui/SubTitle/styles.ts
+++ src/components/ui/SubTitle/styles.ts
@@ -1,35 +1,31 @@
 import styled from "styled-components";
+import { mq } from "../../../utils/responsive/breakpoints";
 
 export const LabelContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
 
-  min-width: 22vw;
-
-  height: 2rem;
-
   padding: 0.5rem;
+
+  margin-bottom: 2rem;
 
   background-color: var(--secundary-text-color);
   border-radius: 0.25rem;
 
-  @media (max-width: 400px) {
-    min-width: 20vw;
-
-    height: 1.5rem;
-  }
+  ${mq({
+    minWidth: ["20vw", null, "22.5vw"],
+    height: ["1.5rem", null, "2rem"],
+  })}
 `;
 
 export const LabelText = styled.span`
-  font-size: 1.5rem;
   font-weight: bold;
-
   white-space: nowrap;
 
   color: var(--background-color);
 
-  @media (max-width: 400px) {
-    font-size: 1rem;
-  }
+  ${mq({
+    fontSize: ["1rem", null, "1.5rem"],
+  })}
 `;
