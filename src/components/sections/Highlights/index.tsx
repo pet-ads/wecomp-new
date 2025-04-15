294b5ee1a63d93aaa32f2c7629dc9d7a44e0aaf9
vmacena <macena.v@aluno.ifsp.edu.br>
2025-04-15 12:44:54 -0300

    refactor: simplify highlights and programming components by removing unused date checks and upcoming cards

--- src/components/sections/Highlights/index.tsx
+++ src/components/sections/Highlights/index.tsx
@@ -2,33 +2,24 @@ import { Section } from "../../Section";
 import SubTitle from "../../ui/SubTitle";
 import ArrowButton from "../../ui/ArrowButton";
 import Slider from "../../Slider";
-import UpcommingCard from "../../ui/SoonCard";
 import Card from "./subcomponents/Card";
-
-import useCheckDate from "../../../hooks/references/useCheckDate";
 
 import highlightsContent from "../../../assets/content/highlights";
 
 import { HighlightsCardContainer, HighlightsContent } from "./styles";
 
 export default function Highlights() {
-  const checkDate = useCheckDate();
-
   return (
     <Section sectionId="Highlights">
       <HighlightsContent>
         <SubTitle children="Destaques" />
         <HighlightsCardContainer>
-          {checkDate ? (
-            <Slider
-              items={highlightsContent}
-              renderItem={(highlight) => (
-                <Card key={highlight.title} {...highlight} />
-              )}
-            />
-          ) : (
-            <UpcommingCard />
-          )}
+          <Slider
+            items={highlightsContent}
+            renderItem={(highlight) => (
+              <Card key={highlight.title} {...highlight} />
+            )}
+          />
         </HighlightsCardContainer>
         <ArrowButton sectionId="Locations" />
       </HighlightsContent>
--- src/components/sections/Programming/index.tsx
+++ src/components/sections/Programming/index.tsx
@@ -2,11 +2,9 @@ import { Section } from "../../Section";
 import ArrowButton from "../../ui/ArrowButton";
 import SubTitle from "../../ui/SubTitle";
 import Slider from "../../Slider";
-import UpcommingCard from "../../ui/SoonCard";
 import Card from "./subcomponents/Card";
 import CardProjeto from "./subcomponents/Modal";
 
-import useCheckDate from "../../../hooks/references/useCheckDate";
 import useIsMobile from "../../../hooks/window/Mobile";
 
 import programmingContent from "../../../assets/content/programming";
@@ -14,7 +12,6 @@ import programmingContent from "../../../assets/content/programming";
 import { ProgrammingCardContainer, ProgrammingContent } from "./styles";
 
 export default function Programming() {
-  const checkDate = useCheckDate();
   const isMobile = useIsMobile();
 
   return (
@@ -22,20 +19,16 @@ export default function Programming() {
       <ProgrammingContent>
         <SubTitle children="Programação" />
         <ProgrammingCardContainer>
-          {checkDate ? (
-            <Slider
-              items={programmingContent}
-              renderItem={(event) =>
-                isMobile ? (
-                  <CardProjeto key={event.name} {...event} />
-                ) : (
-                  <Card key={event.name} {...event} />
-                )
-              }
-            />
-          ) : (
-            <UpcommingCard />
-          )}
+          <Slider
+            items={programmingContent}
+            renderItem={(event) =>
+              isMobile ? (
+                <CardProjeto key={event.name} {...event} />
+              ) : (
+                <Card key={event.name} {...event} />
+              )
+            }
+          />
         </ProgrammingCardContainer>
         <ArrowButton sectionId="Marathon" />
       </ProgrammingContent>
