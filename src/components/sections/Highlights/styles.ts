7ce5ad54d8177438ee7127624d7492433d090fd1
vmacena <macena.v@aluno.ifsp.edu.br>
2025-04-18 07:44:08 -0300

    refactor: simplify highlights component

--- src/components/sections/Highlights/styles.ts
+++ src/components/sections/Highlights/styles.ts
@@ -1,17 +1,12 @@
 import styled from "styled-components";
-
 import { SectionContent } from "../../../styles/SectionContainer";
 
 export const HighlightsContent = styled(SectionContent)`
   background-color: var(--gray-200);
-`;
 
-export const HighlightsCardContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
 
-  width: 100%;
-
-  min-height: 40vh;
+  padding: 4rem 1rem;
 `;
--- src/components/sections/Highlights/subcomponents/CardMain/index.tsx
+++
@@ -1,7 +0,0 @@
-import { Container } from "./styles";
-
-import { CardMainProps } from "./types";
-
-export default function CardMain({ content }: CardMainProps) {
-  return <Container>{content}</Container>;
-}
