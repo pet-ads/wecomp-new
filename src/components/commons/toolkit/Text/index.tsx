75884c468de323437d7fd24b72fd4cd60fc749b6
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-04-22 08:54:24 -0300

    refactor: remove Text compoenent

--- src/components/commons/toolkit/Text/index.tsx
+++
@@ -1,6 +0,0 @@
-import { TextContainerProps } from "./types.ts";
-import { TextContainer } from "./styles.ts";
-
-export default function Text({ children, ...props }: TextContainerProps) {
-  return <TextContainer {...props}>{children}</TextContainer>;
-}
