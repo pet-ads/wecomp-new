9545d8d07eb45445632b6999578a90113d242f80
KG-S <k.gabriel@aluno.ifsp.edu.br>
2025-04-29 12:02:23 -0300

    refactor: Separating the logos in the organizer section

--- src/components/sections/Organization/index.tsx
+++ src/components/sections/Organization/index.tsx
@@ -1,12 +1,14 @@
 import { Section } from "../../commons/structure/Section";
 import organizationContent from "../../../assets/content/organization";
-
-import { Container, DirectorLogo } from "./styles";
+import { Container, DirectorLogo, GroupsContainer} from "./styles";
 
 export default function Directors() {
   const handleRedirectOnClick = (url: string) => {
     window.open(url, "_blank");
   };
+
+  const realizadores = organizationContent.filter(org => org.type === "realizador");
+  const organizadores = organizationContent.filter(org => org.type === "organizador");
 
   return (
     <Section
@@ -14,17 +16,34 @@ export default function Directors() {
       currentSectionId="Organization"
       nextSectionId="Programming"
     >
-      <Container>
-        {organizationContent.map((director, index) => (
-          <DirectorLogo
-            key={index}
-            src={director.logoPath}
-            alt={`Logo da organização ${director.name}`}
-            width={director.width}
-            onClick={() => handleRedirectOnClick(director.link)}
-          />
-        ))}
-      </Container>
+      <GroupsContainer>
+        <Container>
+          <h2>Realizadores</h2>
+          {realizadores.map((director, index) => (
+            <DirectorLogo
+              key={index}
+              src={director.logoPath}
+              alt={`Logo da organização ${director.name}`}
+              width={director.width}
+              onClick={() => handleRedirectOnClick(director.link)}
+            />
+          ))}
+        </Container>
+
+        
+        <Container>
+          <h2>Organizadores</h2>
+          {organizadores.map((director, index) => (
+            <DirectorLogo
+              key={index}
+              src={director.logoPath}
+              alt={`Logo da organização ${director.name}`}
+              width={director.width}
+              onClick={() => handleRedirectOnClick(director.link)}
+            />
+          ))}
+        </Container>
+      </GroupsContainer>
     </Section>
   );
 }
