e8852acb3c26b982e12c97984138e4b8320a06f7
KG-S <k.gabriel@aluno.ifsp.edu.br>
2025-05-04 02:10:32 -0300

    fix: adjusting the container size

--- src/components/sections/Organization/styles.ts
+++ src/components/sections/Organization/styles.ts
@@ -2,20 +2,31 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   display: flex;
-  flex-direction: column;
+  justify-content: center;
   align-items: center;
+  gap: 1rem;
+  flex-direction: column;
+  
 
 `;
+
+export const Content = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 2.5rem;
+
+`
 
 export const GroupsContainer = styled.div`
   width: 100%;
   height: 100%;
 
+  
   display: flex;
-  justify-content: center;
-  align-items: flex-start;
-  gap: 2rem; 
-  flex-wrap: wrap; 
+  justify-content: space-around;
+  align-items: center; 
+  gap: 2rem;
 `;
 
 export const DirectorLogo = styled.img<{ width?: string }>`
@@ -25,6 +36,6 @@ export const DirectorLogo = styled.img<{ width?: string }>`
 
   object-fit: contain;
   overflow: hidden;
-
+  
   cursor: pointer;
 `;
