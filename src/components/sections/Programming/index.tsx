0b87d46138b4ed5c2d2f921647c0d3ec6c1f08f5
KG-S <k.gabriel@aluno.ifsp.edu.br>
2025-05-11 01:06:21 -0300

    fix: Changing the modal using Ceateportal

--- src/components/sections/Programming/index.tsx
+++ src/components/sections/Programming/index.tsx
@@ -1,17 +1,18 @@
 import { Section } from "../../commons/structure/Section";
-// import Slider from "../../commons/toolkit/Slider";
-// import Card from "./subcomponents/Card";
-// import CardProjeto from "./subcomponents/Modal";
 
-// import useIsMobile from "../../../hooks/window/Mobile";
+import Slider from "../../commons/toolkit/Slider";
+import Card from "./subcomponents/Card";
+import CardProjeto from "./subcomponents/Modal";
 
-// import programmingContent from "../../../assets/content/programming";
+import useIsMobile from "../../../hooks/window/Mobile";
+
+import programmingContent from "../../../assets/content/programming";
 
 import { Container } from "./styles";
-import SoonCard from "../../commons/toolkit/SoonCard";
+//import SoonCard from "../../commons/toolkit/SoonCard";
 
 export default function Programming() {
-  // const isMobile = useIsMobile();
+  const isMobile = useIsMobile();
 
   return (
     <Section
@@ -20,7 +21,7 @@ export default function Programming() {
       nextSectionId="Marathon"
     >
       <Container>
-        {/* <Slider
+        <Slider
           items={programmingContent}
           renderItem={(event) =>
             isMobile ? (
@@ -29,8 +30,8 @@ export default function Programming() {
               <Card key={event.name} {...event} />
             )
           }
-        /> */}
-        <SoonCard/>
+        />
+        
       </Container>
     </Section>
   );
