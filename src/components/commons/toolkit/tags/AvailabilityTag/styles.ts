10d84dbe124a450d4d550205ef14ba83981e0bba
KG-S <k.gabriel@aluno.ifsp.edu.br>
2025-05-20 10:23:15 -0300

    fix: adjusting the size of the text according to the screen size

--- src/components/commons/toolkit/tags/AvailabilityTag/styles.ts
+++ src/components/commons/toolkit/tags/AvailabilityTag/styles.ts
@@ -1,8 +1,16 @@
 import styled from "styled-components";
+import { mq } from "../../../../../utils/responsive/breakpoints";
 
 import { TagContainer } from "../../../../../styles/TagContainer";
 
 export const Container = styled(TagContainer)<{
   backgroundColor: string;
   textColor: string;
-}>``;
+  
+}>`
+  ${mq({
+        width: ["4rem", "4rem", "4rem", "4rem", "4rem", "4rem", "6rem"],
+        height: ["2rem", "2rem", "2rem", "2rem", "2rem", "2rem", "3rem"],
+        fontSize: ["0.4rem", "0.4rem", "0.4rem", "0.4rem", "0.5rem", "0.5rem", "0.7rem"],
+      })}
+`;
