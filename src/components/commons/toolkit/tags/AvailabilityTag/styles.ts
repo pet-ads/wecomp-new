07f0b9f32164b0b31d0dbca933cacd73d76ab470
KG-S <k.gabriel@aluno.ifsp.edu.br>
2025-05-25 01:25:29 -0300

    fix: Adjusting the sources 2.0

--- src/components/commons/toolkit/tags/AvailabilityTag/styles.ts
+++ src/components/commons/toolkit/tags/AvailabilityTag/styles.ts
@@ -9,8 +9,8 @@ export const Container = styled(TagContainer)<{
   
 }>`
   ${mq({
-        width: ["4rem", "4rem", "4rem", "4rem", "4rem", "4rem", "6rem"],
-        height: ["2rem", "2rem", "2rem", "2rem", "2rem", "2rem", "3rem"],
+        width: ["5rem", "5rem", "5rem", "5rem", "5rem", "5rem", "6rem"],
+        height: ["1rem", "1rem", "1rem", "1rem", "1rem", "1rem", "2rem"],
         fontSize: ["0.5rem", "0.5rem", "0.5rem", "0.5rem", "0.6rem", "0.6rem", "0.7rem"],
       })}
 `;
