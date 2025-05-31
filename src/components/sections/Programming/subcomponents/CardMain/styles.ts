3b89e6451f96efb4946de2c96fdc5c6ef46ba2aa
KG-S <k.gabriel@aluno.ifsp.edu.br>
2025-05-31 05:22:12 -0300

    fix: adjusting the size of the cardmain

--- src/components/sections/Programming/subcomponents/CardMain/styles.ts
+++ src/components/sections/Programming/subcomponents/CardMain/styles.ts
@@ -16,12 +16,17 @@ export const Container = styled.div`
 
   text-align: justify;
 
-
   @media (max-height: 750px) and (min-width: 600px) and (min-height: 769px) {
-  width: 100%;
-  min-height: 2rem;
-  max-height: 2rem;
-}
+    width: 100%;
+    min-height: 2rem;
+    max-height: 2rem;
+  }
+
+  @media (max-height: 684px) {
+    width: 100%;
+    min-height: 4.3rem;
+    max-height: 4.3rem;
+  }
 
   @media (max-width: 600px) {
     width: 100%;
