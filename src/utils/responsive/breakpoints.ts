db664bc91329190fbbb8ee8a8e823403b2ebc8c8
KG-S <k.gabriel@aluno.ifsp.edu.br>
2025-06-03 08:07:05 -0300

    fix: setting size bases

--- src/utils/responsive/breakpoints.ts
+++ src/utils/responsive/breakpoints.ts
@@ -1,7 +1,7 @@
 import facepaint from "facepaint";
 
 export const breakpoints = [
-  320, 375, 414, 480, 600, 768, 900, 1024, 1280, 1440, 1600, 1920,
+  319, 375, 414, 480, 600, 768, 900, 1024, 1280, 1440, 1600, 1920,
 ];
 
 export const mq = facepaint(
