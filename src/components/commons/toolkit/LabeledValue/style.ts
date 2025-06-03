4c7a6f8a9d90e85c54cce0465c24dee96592b740
KG-S <k.gabriel@aluno.ifsp.edu.br>
2025-06-03 08:05:50 -0300

    fix: Rejecting font size

--- src/components/commons/toolkit/LabeledValue/style.ts
+++ src/components/commons/toolkit/LabeledValue/style.ts
@@ -4,11 +4,11 @@ import { mq } from "../../../..//utils/responsive/breakpoints";
 export const LabeledValueContainer = styled.div`
   display: flex;
 
-  gap: 1rem;
+  gap: 0.5rem;
   
   font-size: 1.25rem;
 
-  ${mq({  
-          fontSize: ["0.75rem", "0.90rem", "0.925", "0.95rem", "1rem", "1.1rem", "1.25rem"],
-        })}
+  ${mq({
+    fontSize: ["0.6rem", "0.6rem", "0.6rem", "0.7rem", "0.7rem", "0.9rem", "1rem"],
+  })}
 `;
