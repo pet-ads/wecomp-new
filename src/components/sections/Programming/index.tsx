a110c36b9abfecc130e6b16cf2d39c8db9f6ceb3
KG-S <k.gabriel@aluno.ifsp.edu.br>
2025-06-18 21:41:00 -0300

    fix: placing the carousel

--- src/components/sections/Programming/index.tsx
+++ src/components/sections/Programming/index.tsx
@@ -1,14 +1,21 @@
 import { Section } from "../../commons/structure/Section";
 
+import Slider from "../../commons/toolkit/Slider";
+import Card from "./subcomponents/Card";
+import CardProjeto from "./subcomponents/Modal";
+
+import useIsMobile from "../../../hooks/window/Mobile";
+import useIsMobileHeight from "../../../hooks/window/MobileHeight";
+
+import programmingContent from "../../../assets/content/programming";
+
 import { Container } from "./styles";
-import SoonCard from "../../commons/toolkit/SoonCard";
-// import useIsMobile from "../../../hooks/window/Mobile";
-// import useIsMobileHeight from "../../../hooks/window/MobileHeight";
+//import SoonCard from "../../commons/toolkit/SoonCard";
 
 export default function Programming() {
-  // const isMobile = useIsMobile();
-  // const isMobileHeight= useIsMobileHeight();
-  // const shouldUseMobileLayout = isMobile || isMobileHeight;
+  const isMobile = useIsMobile();
+  const isMobileHeight= useIsMobileHeight();
+  const shouldUseMobileLayout = isMobile || isMobileHeight;
 
   return (
     <Section
@@ -17,9 +24,18 @@ export default function Programming() {
       nextSectionId="Marathon"
     >
       <Container>
-        <SoonCard />
+        <Slider
+          items={programmingContent}
+          renderItem={(event) =>
+            shouldUseMobileLayout ? (
+              <CardProjeto key={event.name} {...event} />
+            ) : (
+              <Card key={event.name} {...event} />
+            )
+          }
+        />
+        
       </Container>
     </Section>
   );
 }
-
